4743c557c53cc90529387b1ad60544b9b16206f9
Jamie Kennedy <jamiekennedy97@gmail.com>
2020-10-23 11:42:24 +0100

    Added sanpile class with update rules and printing

--- Abelian_Sandpile_Model/Abelian_Sandpile_Model/Program.cs
+++ Abelian_Sandpile_Model/Abelian_Sandpile_Model/Program.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using System.Diagnostics.Tracing;
+using System.Threading;
 
 namespace Abelian_Sandpile_Model {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Sandpile sp = new Sandpile(20, 20);
+            int centreX = 9;
+            int centreY = 9;
+            int counter = 0;
+
+            while (counter < 1000) {
+                counter++;
+                Console.Clear();
+                sp.PrintBoard();
+                sp.AddSand(centreX, centreY);
+                Thread.Sleep(100);
+            }
         }
     }
 }
---
+++ Abelian_Sandpile_Model/Abelian_Sandpile_Model/Sandpile.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Abelian_Sandpile_Model {
+    class Sandpile {
+        public int width { get; set; }
+        public int height { get; set; }
+
+        public int[,] board;
+
+        public Sandpile(int width, int height) {
+            this.width = width;
+            this.height = height;
+            Init();
+        }
+
+        private void Init() {
+            board = new int[height, width];
+        }
+
+        public void AddSand(int x, int y) {
+            if (IsInBounds(x, y)) {
+                board[y, x]++;
+                Update();
+            } else {
+                Console.WriteLine("Provided Coordinates are not within the bounds of the board");
+            }
+        }
+
+        private void Update() {
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    if (board[i, j] > 3) {
+                        board[i, j] -= 4;
+
+                        if (IsInBounds(j - 1, i)) {
+                            AddSand(j - 1, i);
+                        }
+
+                        if (IsInBounds(j + 1, i)) {
+                            AddSand(j + 1, i);
+                        }
+
+                        if (IsInBounds(j, i - 1)) {
+                            AddSand(j, i - 1);
+                        }
+
+                        if (IsInBounds(j, i + 1)) {
+                            AddSand(j, i + 1);
+                        }
+                    }
+                }
+
+            }
+        }
+
+        public void PrintBoard() {
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    Console.Write($"{board[i, j]} ");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine();
+        }
+
+        private bool IsInBounds(int x, int y) {
+            if (x >= 0 && x < width) {
+                if (y >= 0 && y < height) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool IsStable() {
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    if (board[i, j] > 3) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
