4ea6345425203233a79d9499242e9ceba7e9c0d8
Jamie Kennedy <jamiekennedy97@gmail.com>
2020-10-23 11:00:09 +0100

    Updated gitignore

---
+++ Abelian_Sandpile_Model/Abelian_Sandpile_Model/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Abelian_Sandpile_Model {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
